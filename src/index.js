f8947fecefce8dc39fe8024a15b0cbc40fe073fe
Jerico Radin <jericoradin@Jericos-MacBook-Pro.local>
2021-08-26 16:01:53 -1000

    React Component Life Cycle

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+setTimeout(() => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'));}, 10000);
