7b70217ee7b3a4707ba0de166ab84e529ab1b411
Jerico Radin <jericoradin@Jericos-MacBook-Pro-Work.local>
2021-10-14 10:23:35 -1000

    Cleaner code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class Welcome extends React.Component {
     render() {
-        return <div><h1>Hello, {this.props.name}</h1>
-        <p>You're {this.props.age}, right?</p></div>
-    };
+        return (
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+                <p>You're {this.props.age}, right?</p>
+            </div>
+        );
+    }
 }
 class App extends React.Component {
     render() {
@@ -12,21 +16,28 @@ class App extends React.Component {
         <Welcome name="Ben" age="12"/>
         <Welcome name="Ten" age ="18"/>
         </div>
-    }
+    };
 }
 
 function WelcomeFunction(props){
-    return <div>
+    return (
+        <div>
             <h1>Hello, {props.name}</h1>
             <p>You're {props.age}, right?</p>
         </div>
+    );
 }
 
 function Triple(){
-    return <div>
-        <WelcomeFunction name="Benny" age="12"/>
-        <WelcomeFunction name="Tenny" age ="18"/>
+    return (
+        <div>
+            <WelcomeFunction name="Benny" age="12"/>
+            <WelcomeFunction name="Tenny" age ="18"/>
         </div>
+    );
 }
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(
+    <Triple />,
+    document.getElementById("root")
+);
