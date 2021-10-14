64d74efb62a0e1ef125398669b07e282fa39d7e0
Jerico Radin <jericoradin@Jericos-MacBook-Pro-Work.local>
2021-10-14 10:14:51 -1000

    ES6 class class and function differences

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+class Welcome extends React.Component {
+    render() {
+        return <div><h1>Hello, {this.props.name}</h1>
+        <p>You're {this.props.age}, right?</p></div>
+    };
+}
+class App extends React.Component {
+    render() {
+        return <div>
+        <Welcome name="Ben" age="12"/>
+        <Welcome name="Ten" age ="18"/>
+        </div>
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function WelcomeFunction(props){
+    return <div>
+            <h1>Hello, {props.name}</h1>
+            <p>You're {props.age}, right?</p>
+        </div>
+}
+
+function Triple(){
+    return <div>
+        <WelcomeFunction name="Benny" age="12"/>
+        <WelcomeFunction name="Tenny" age ="18"/>
+        </div>
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
