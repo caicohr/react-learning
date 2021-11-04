0c46ec8caed7b29ca03ca6ff62828484211c2110
Jerico Radin <jericoradin@Jericos-MacBook-Pro-Work.local>
2021-11-04 13:08:48 -1000

    Composition part 1

--- src/index.js
+++ src/index.js
@@ -411,10 +411,32 @@ function tryConvert(temperature, convert) {
     return rounded.toString();
 }
 
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return(
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                Welcome
+            </h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+            </p>
+        </FancyBorder>
+    );
+}
+
 class App extends React.Component {//second to run
     render() {//find the Welcome component then the clock component will run
         return (
             <div>
+                <WelcomeDialog />
                 <p>Own Calclulator Style</p>
                 <OwnCalculator />
                 <p>React Style</p>
